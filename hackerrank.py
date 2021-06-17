5506fccec18c172d2c0581f5a401222c438d7b29
Abdelilah Bouizzem <abdelilah.bouizzem@gmail.com>
2021-06-17 15:14:12 +0100

    Number Line Jumps

--- hackerrank.py
+++ hackerrank.py
@@ -124,6 +124,19 @@ def gradingStudents(grades):
     return list_grades
 
 
+#Number Line Jumps
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    if x2> x1 and v2>= v1:
+        return 'NO'    
+    
+    elif (x1-x2) % (v2-v1) == 0:
+        return 'YES'
+
+    else:
+        return 'NO'
+
+
 #Apple and Orange
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     number_apples = 0
