0c0f32c8019f7a06d0c80ddab004f197e9e7264d
Abdelilah Bouizzem <abdelilah.bouizzem@gmail.com>
2021-06-25 09:19:29 +0100

    Day of the Programmer if-else

--- hackerrank.py
+++ hackerrank.py
@@ -248,12 +248,6 @@ def dayOfProgrammer(year):
     if year == 1918:
         return '26.09.1918'
     elif 1700<= year<=1917 :
-        if year % 4 == 0:
-            return f'12.09.{year}'
-        else :
-            return f'13.09.{year}'
+        return f'12.09.{year}' if year % 4 == 0 else  f'13.09.{year}'
     elif 1918 < year:
-        if year %400 ==0 or (year %4 ==0 and year % 100 !=0):
-            return f'12.09.{year}'
-        else :
-            return f'13.09.{year}'
+        return f'12.09.{year}' if year %400 ==0 or (year %4 ==0 and year % 100 !=0) else f'13.09.{year}'
