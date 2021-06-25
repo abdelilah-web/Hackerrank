7a68384f404e4efbc383ceb190ed97ec5d695b4e
Abdelilah Bouizzem <abdelilah.bouizzem@gmail.com>
2021-06-25 09:10:52 +0100

    Day of the Programmer

--- hackerrank.py
+++ hackerrank.py
@@ -241,3 +241,19 @@ def migratoryBirds(arr):
                 z += 1
                 Dic[x]= z
     return max(Dic, key=Dic.get)  
+
+
+#Day of the Programmer
+def dayOfProgrammer(year):
+    if year == 1918:
+        return '26.09.1918'
+    elif 1700<= year<=1917 :
+        if year % 4 == 0:
+            return f'12.09.{year}'
+        else :
+            return f'13.09.{year}'
+    elif 1918 < year:
+        if year %400 ==0 or (year %4 ==0 and year % 100 !=0):
+            return f'12.09.{year}'
+        else :
+            return f'13.09.{year}'
