e8f2525cc95c4599650e9f5f4cca2f4cad24562f
Abdelilah <abdelilah.bouizzem@gmail.com>
2021-07-03 10:08:02 +0100

    Forming a Magic Square

--- hackerrank.py
+++ hackerrank.py
@@ -342,3 +342,27 @@ def catAndMouse(x, y, z):
         return 'Cat B'
     if abs(x-z)==abs(y-z):
         return 'Mouse C' 
+
+
+#Forming a Magic Square
+def formingMagicSquare(s):
+    base = [
+        [4,9,2,3,5,7,8,1,6],
+        [4,3,8,9,5,1,2,7,6],
+        [2,9,4,7,5,3,6,1,8],
+        [2,7,6,9,5,1,4,3,8],
+        [8,1,6,3,5,7,4,9,2],
+        [8,3,4,1,5,9,6,7,2],
+        [6,7,2,1,5,9,8,3,4],
+        [6,1,8,7,5,3,2,9,4],
+    ]
+    s = s[0]+s[1]+s[2]
+    count = 100        
+    for square in base:
+        a =0
+        for i,j in zip(square,s):
+            a += abs(i-j)
+        if a<count :
+            count = a
+    return count        
+            
